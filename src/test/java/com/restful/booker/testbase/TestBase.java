4ae02ee2a7969ce843f6e3adcbb3fee83487b3d1
virrenpatel <virenpatel24611@gmail.com>
2022-12-27 00:44:27 +0000

    restful

---
+++ src/test/java/com/restful/booker/testbase/TestBase.java
@@ -0,0 +1,15 @@
+package com.restful.booker.testbase;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+public class TestBase {
+
+    @BeforeClass
+    public static void inIt() {
+        RestAssured.baseURI="https://restful-booker.herokuapp.com";
+
+        // RestAssured.basePath="/api";
+
+    }
+}
